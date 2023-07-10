f41189aa27e219e69e12b291f085d8db5295c944
Tetiana <tania.pokhylko@gmail.com>
2023-07-10 23:25:32 +0300

    hw-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
+import Profile from "./Profile/Profile";
+import user from "../user.json"
+
 export const App = () => {
-  return (
-    <div
+  return <div
       style={{
         height: '100vh',
         display: 'flex',
@@ -10,7 +12,16 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={ user.stats.likes} />
     </div>
-  );
+  
 };
+
+
